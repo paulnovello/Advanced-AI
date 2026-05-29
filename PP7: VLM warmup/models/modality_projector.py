bc63369b6d52dc6a8dfea1c0c08d00d4347acf79
bougachaY <yassinebougacha@gmail.com>
2026-05-29 14:29:45 +0200

    m

--- PP7: VLM warmup/models/modality_projector.py
+++ PP7: VLM warmup/models/modality_projector.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 class ModalityProjector(nn.Module):
     """Student implementation target for the modality projector exercise."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vision_dim, llm_dim):
         """Placeholder initializer for the exercise implementation.
 
         Args:
@@ -14,5 +14,14 @@ class ModalityProjector(nn.Module):
             **kwargs: Keyword arguments the student-defined projector may need.
         """
         super().__init__()
+        self.projector = nn.Sequential(
+            nn.Linear(vision_dim, llm_dim),
+            nn.GELU(),
+            nn.Linear(llm_dim, llm_dim))
 
     # TODO implement the modality projector.
+    def forward(self, x):
+         
+        # TODO implement the forward pass of the modality projector.
+        return self.projector(x)
+
